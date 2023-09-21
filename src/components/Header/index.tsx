fe2ba0f850093a73c1d8132082f8941c6ceb859b
Wilian Pereira <wilian_pereira92@outlook.com>
2023-09-20 21:36:54 -0300

    feat: criando o componente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+function Header() {
+  return (
+    <header className={styles.container}>
+      <h1>Reflex Test Game</h1>
+    </header>
+  );
+}
+
+export default Header;
